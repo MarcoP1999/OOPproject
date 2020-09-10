33e0b84a339f8d1d6bf083c6d280ee3e96e17ac2
MarcoP1999 <69671622+MarcoP1999@users.noreply.github.com>
2020-09-10 22:48:00 +0200

    Update Filtri.java

--- src/main/java/univpm/progetto/FiltrieStatistiche/Filtri.java
+++ src/main/java/univpm/progetto/FiltrieStatistiche/Filtri.java
@@ -87,7 +87,7 @@ public class Filtri {
 	 * @return boolean che sta ad indicare se l'elemento rispetta altezza e laghezza richieste 
 	 */
 	
-	public Boolean FiltroDimAltLargh(Long alt, Long largh, Long altObj, Long larghObj, String operatore) {
+	public Boolean FiltroAltLargh(Long alt, Long largh, Long altObj, Long larghObj, String operatore) {
 		
 		if (altObj == null) {
 			altObj = (long) 0;
